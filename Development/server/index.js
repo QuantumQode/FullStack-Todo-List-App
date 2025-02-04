300c2f4f83ec205f379cb70e7f8043d969b97e84
Pouria <pouria.ghasemi@yahoo.co.uk>
2025-02-04 09:32:18 +0000

    backend session/cookies

--- Development/server/index.js
+++ Development/server/index.js
@@ -12,14 +12,43 @@ const app = express();
 // The cors variable imports the cors library, which allows the server to accept requests from the client.
 const cors = require('cors');
 
+// Encryption libraries
 // The bcrypt library is used to hash passwords before storing them in the database.
 const bcrypt = require('bcrypt');
 const saltRounds = 10;
 
+// Authentication libraries
+// The bodyParser library is used to parse the body of incoming requests.
+const bodyParser = require('body-parser');
+// The cookieParser library is used to parse cookies in the request headers.
+const cookieParser = require('cookie-parser');
+// The session library is used to create sessions for users.
+const session = require('express-session');
+
 
 // The app.use() function tells the server to use the express.json() and cors() middleware.
 app.use(express.json()); 
-app.use(cors());
+app.use(cors({
+    origin: ['http://localhost:3000'],
+    methods: ['GET', 'POST'],
+    credentials: true
+}));
+
+// The app.use() function tells the server to use the bodyParser, cookieParser, and session middleware.
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+
+// The app.use() function tells the server to use the session middleware with the specified options.
+app.use(session({
+    key: 'userId',
+    secret: 'QuantumQoder1!',
+    resave: false,
+    saveUninitialized: false,
+    cookie: {
+        expires: 60 * 60 * 24 * 1000,
+    },
+}));
 
 
 //create connection to database
